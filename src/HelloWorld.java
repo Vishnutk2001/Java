3b2d6d00de602da9c9054d1f8b58df5021c9f9a6
Vishnutk2001 <t.k.vishnu2001@gmail.com>
2022-03-14 12:25:55 +0530

    examples

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Integer a=10;
+        if (a%2==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+
+    }
+}
